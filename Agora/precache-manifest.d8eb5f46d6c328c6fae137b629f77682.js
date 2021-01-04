a79ab01d48b91833fb5415d721f41b336e956843
Jefry <jefry.ted@vcube.co.jp>
2021-01-04 15:57:22 +0700

    add Agora + TfLite

---
+++ Agora/precache-manifest.d8eb5f46d6c328c6fae137b629f77682.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5831b341b5d2163a48d9569b33ca89c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa5d08ff604a49b3444",
+    "url": "/static/css/main.c4d38b88.chunk.css"
+  },
+  {
+    "revision": "e2040608678cf80fc5b2",
+    "url": "/static/js/2.e0afaa65.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/static/js/2.e0afaa65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45edb3a9ebb12badea133ca538824bc2",
+    "url": "/static/js/TfLiteWorker.0c974880.worker.js"
+  },
+  {
+    "revision": "0fa5d08ff604a49b3444",
+    "url": "/static/js/main.bf2133ea.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  }
+]);
