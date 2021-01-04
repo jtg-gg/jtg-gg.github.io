95220c02d2d8708dc10b75df9aeb4f53aba89b15
Jefry <jefry.ted@vcube.co.jp>
2021-01-04 16:11:22 +0700

    fix Agora react app absolute path issue

--- Agora/service-worker.js
+++ Agora/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8eb5f46d6c328c6fae137b629f77682.js"
+  "/Agora/precache-manifest.67a9f09e69587318cabe1159ca9045f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Agora/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
