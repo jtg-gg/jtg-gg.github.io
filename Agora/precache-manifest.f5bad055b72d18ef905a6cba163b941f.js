3c8d783a09fdc1ea69a0764816d828b72c1ab4f0
Jefry <jefry.ted@vcube.co.jp>
2021-01-04 16:03:35 +0700

    fix Agora react app absolute path issue

---
+++ Agora/precache-manifest.f5bad055b72d18ef905a6cba163b941f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789172d1ee7ff5ed65ede92bc0a9a0fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64dbc40312ca2d996cde",
+    "url": "./static/css/main.c4d38b88.chunk.css"
+  },
+  {
+    "revision": "9cbcd9ade5afb83ded24",
+    "url": "./static/js/2.174a3242.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "./static/js/2.174a3242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02e674b99304fcf3faacb9b0787a29b",
+    "url": "./static/js/TfLiteWorker.bf21cf03.worker.js"
+  },
+  {
+    "revision": "64dbc40312ca2d996cde",
+    "url": "./static/js/main.a3af29f2.chunk.js"
+  },
+  {
+    "revision": "7beddeb4223c250b4079",
+    "url": "./static/js/runtime-main.da66a749.js"
+  }
+]);
