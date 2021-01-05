f190198cc41cff8e127468acf7d4edc6dcd48ed8
Jefry <jefry.ted@vcube.co.jp>
2021-01-05 09:50:21 +0700

    Agora fix webgl warning, add option to set appid from url param

---
+++ Agora/precache-manifest.e355c1aca48f307f0f4352162805f5fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c643466a13e403724211e4f7bcfd5e2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a78da7c71b1583e360b3",
+    "url": "./static/css/main.c4d38b88.chunk.css"
+  },
+  {
+    "revision": "9cbcd9ade5afb83ded24",
+    "url": "./static/js/2.174a3242.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "./static/js/2.174a3242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02e674b99304fcf3faacb9b0787a29b",
+    "url": "./static/js/TfLiteWorker.bf21cf03.worker.js"
+  },
+  {
+    "revision": "a78da7c71b1583e360b3",
+    "url": "./static/js/main.0d208873.chunk.js"
+  },
+  {
+    "revision": "7beddeb4223c250b4079",
+    "url": "./static/js/runtime-main.da66a749.js"
+  }
+]);
--- Agora/service-worker.js
+++ Agora/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Agora/precache-manifest.67a9f09e69587318cabe1159ca9045f6.js"
+  "./precache-manifest.e355c1aca48f307f0f4352162805f5fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Agora/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
