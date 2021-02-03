22cf47143903052bb4f35cfcae35cc66057e6ecd
Jefry <jefry.ted@vcube.co.jp>
2021-02-03 08:52:37 +0700

    add AgoraRecorder

---
+++ AgoraRecorder/precache-manifest.cef6bcef6b8b52ae68cdfa346bc32e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c17f106d99d235bf58f227aa176d45f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9fe36648b2b50c10bddf",
+    "url": "./static/css/main.c4d38b88.chunk.css"
+  },
+  {
+    "revision": "7efe5e1e57049282079a",
+    "url": "./static/js/2.d0adac3e.chunk.js"
+  },
+  {
+    "revision": "0474c8124a6044c4ca61f82a582811ab",
+    "url": "./static/js/2.d0adac3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe36648b2b50c10bddf",
+    "url": "./static/js/main.cb123e32.chunk.js"
+  },
+  {
+    "revision": "7beddeb4223c250b4079",
+    "url": "./static/js/runtime-main.da66a749.js"
+  }
+]);
